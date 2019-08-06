74be77777518043ead87a8a1de05638753ddbcc7
Cayque Freitas <cayquefreitas@gmail.com>
2019-08-06 19:30:20 -0300

    desktop finished

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,23 +1,29 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
-  width: 100%;
+  width: 100vw;
   display: flex;
   background-color: #fff;
   justify-content: space-between;
   align-items: center;
-  padding: 35px 170px 20px;
+  padding: 1.8vw 8.8vw 1vw;
+  img {
+    width: 21vw;
+  }
   div {
-    width: 30%;
+    width: 24.6vw;
   }
   nav {
-    width: 60%;
+    width: 49.3vw;
     text-transform: uppercase;
     font-weight: 700;
   }
   ul {
     display: flex;
     justify-content: space-between;
+  }
+  li {
+    font-size: 0.9vw;
   }
   a {
     color: #000;
