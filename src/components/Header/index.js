326403f1b186e4a7e1c22f10fa46e7f34583c2e9
Samuel Levy <samuellevy@meshproducoes.com.br>
2019-08-08 06:36:17 -0300

    menu mobile

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "../../assets/logo.png";
-import { Container } from "./styles";
+import { Container, CloseMobile, HeaderBox, Button } from "./styles";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
-export default function Header() {
-  return (
-    <Container>
-      <div>
-        <img src={logo} alt="" />
-      </div>
-      <nav>
-        <ul>
-          <li>
-            <AnchorLink href="#campanha">Campanha</AnchorLink>
-          </li>
-          <li>
-            <AnchorLink href="#questao-saude">Questão de saúde</AnchorLink>
-          </li>
-          <li>
-            <AnchorLink href="#atuacao">Atuação</AnchorLink>
-          </li>
-          <li>
-            <AnchorLink href="#making-of">Making Of</AnchorLink>
-          </li>
-          <li>
-            <AnchorLink href="#contato">Contato</AnchorLink>
-          </li>
-        </ul>
-      </nav>
-    </Container>
-  );
+export default class Header extends Component {
+  state = {
+    show: false
+  };
+  handleButton = e => {
+    e.preventDefault();
+    if (this.state.show) {
+      this.setState({ show: false });
+    } else {
+      this.setState({ show: true });
+    }
+  };
+  render() {
+    return (
+      <Container show={this.state.show} onClick={this.handleButton}>
+        <HeaderBox>
+          <img src={logo} alt="" />
+          <Button onCLick={this.handleButton} />
+        </HeaderBox>
+        <nav>
+          <ul>
+            <li>
+              <AnchorLink href="#campanha" onClick={this.handleButton}>
+                Campanha
+              </AnchorLink>
+            </li>
+            <li>
+              <AnchorLink href="#questao-saude" onClick={this.handleButton}>
+                Questão de saúde
+              </AnchorLink>
+            </li>
+            <li>
+              <AnchorLink href="#atuacao" onClick={this.handleButton}>
+                Atuação
+              </AnchorLink>
+            </li>
+            <li>
+              <AnchorLink href="#making-of" onClick={this.handleButton}>
+                Making Of
+              </AnchorLink>
+            </li>
+            <li>
+              <AnchorLink href="#contato" onClick={this.handleButton}>
+                Contato
+              </AnchorLink>
+            </li>
+          </ul>
+          <CloseMobile onClick={this.handleButton}>x</CloseMobile>
+        </nav>
+      </Container>
+    );
+  }
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -30,6 +30,7 @@ export const Container = styled.header`
     width: 49.3vw;
     text-transform: uppercase;
     font-weight: 700;
+
     @media only screen and (max-width: 768px) {
       width: 12.3vw;
       height: 12vw;
@@ -37,22 +38,77 @@ export const Container = styled.header`
       background-size: 30px;
       background-position: center;
       background-repeat: no-repeat;
+
+      position: fixed;
+      top: 0;
+      right: ${props => (props.show ? "0" : "-100vw")};
+      width: 100%;
+      height: 100%;
+      transition: all 0.5s;
     }
   }
   ul {
     display: flex;
     justify-content: space-between;
     @media only screen and (max-width: 768px) {
+      z-index: 9999;
+      top: 0;
+      left: 0;
+      flex-wrap: wrap;
+      flex-direction: column;
+      background-color: white;
+      padding: 10vw;
+      width: 100%;
+      height: 100%;
+      justify-content: flex-start;
     }
   }
   li {
     font-size: 0.9vw;
     @media only screen and (max-width: 768px) {
       position: relative;
-      right: -200%;
     }
   }
   a {
     color: #000;
+    @media only screen and (max-width: 768px) {
+      font-size: 4.6vw;
+    }
+  }
+`;
+
+export const CloseMobile = styled.div`
+  position: absolute;
+  color: black;
+  font-size: 4vw;
+  z-index: 99999;
+  right: 10vw;
+  top: 10vw;
+  width: auto !important;
+  height: auto !important;
+  @media only screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+
+export const HeaderBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 85.6vw !important;
+  align-items: center;
+`;
+
+export const Button = styled.button`
+  border: none;
+  outline: none;
+  background-color: transparent;
+  color: black;
+  font-size: 4vw;
+  background-image: url(${mobileMenu});
+  background-repeat: no-repeat;
+  width: 35px;
+  height: 35px;
+  @media only screen and (min-width: 768px) {
+    display: none;
   }
 `;
